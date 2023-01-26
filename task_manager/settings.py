41f0a5e094ca5e7562354e085c0a75aafa721c20
Andrey Ivanov <ivnv.xd@gmail.com>
2023-01-26 12:41:38 +0100

    refactor fixtures

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -153,3 +153,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FIXTURE_DIRS = ('task_manager/fixtures/',)
