5fa262d4226c2662d27d689a58de714bd50de842
Andrey Ivanov <ivnv.xd@gmail.com>
2023-01-26 14:15:54 +0100

    add delete protection mixin

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -2,6 +2,7 @@ from django.urls import reverse_lazy
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
+from django.db.models import ProtectedError
 from django.utils.translation import gettext as _
 
 
@@ -15,3 +16,15 @@ class AuthRequiredMixin(LoginRequiredMixin):
             return redirect(reverse_lazy('login'))
 
         return super().dispatch(request, *args, **kwargs)
+
+
+class DeleteProtectionMixin:
+    def post(self, request, *args, **kwargs):
+        try:
+            return super().post(request, *args, **kwargs)
+        except ProtectedError:
+            messages.error(
+                request,
+                _('It is not possible to delete a status because it is in use')
+            )
+            return redirect(reverse_lazy('login'))
