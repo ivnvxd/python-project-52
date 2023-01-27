5f79c7fafe5043e2ea91fc198f467ae29ba6e6b2
Andrey Ivanov <ivnv.xd@gmail.com>
2023-01-27 11:12:57 +0100

    refactor authorisation mixin

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -11,12 +11,11 @@ class AuthRequiredMixin(LoginRequiredMixin):
     Authentication check.
     Restricts access without authentication.
     """
+    auth_message = _('You are not logged in! Please log in.')
+
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            messages.error(
-                request,
-                _('You are not logged in! Please log in.')
-            )
+            messages.error(request, self.auth_message)
             return redirect(reverse_lazy('login'))
 
         return super().dispatch(request, *args, **kwargs)
@@ -34,10 +33,7 @@ class UserPermissionMixin(UserPassesTestMixin):
         return self.get_object() == self.request.user
 
     def handle_no_permission(self):
-        messages.error(
-            self.request,
-            self.permission_message
-        )
+        messages.error(self.request, self.permission_message)
         return redirect(self.permission_url)
 
 
@@ -53,8 +49,29 @@ class DeleteProtectionMixin:
         try:
             return super().post(request, *args, **kwargs)
         except ProtectedError:
-            messages.error(
-                request,
-                self.protected_message
-            )
+            messages.error(request, self.protected_message)
             return redirect(self.protected_url)
+
+
+class AuthorDeletionMixin(UserPassesTestMixin):
+    """
+    Authorisation check.
+    Prohibits deleting an item not by its author.
+    """
+    author_message = None
+    author_url = None
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
+
+    def handle_no_permission(self):
+        messages.error(self.request, self.author_message)
+        return redirect(self.author_url)
+
+    # Another way using dispatch function without UserPassesTestMixin:
+
+    # def dispatch(self, request, *args, **kwargs):
+    #     if request.user.id != self.get_object().author.id:
+    #         messages.error(self.request, self.author_message)
+    #         return redirect(self.author_url)
+    #     return super().dispatch(request, *args, **kwargs)
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.utils.translation import gettext_lazy as _
 from django.contrib.messages.views import SuccessMessageMixin
 
-from task_manager.mixins import AuthRequiredMixin, UserPermissionMixin
+from task_manager.mixins import AuthRequiredMixin, AuthorDeletionMixin
 from task_manager.users.models import User
 from .models import Task
 from .forms import TaskForm
@@ -65,7 +65,7 @@ class TaskUpdateView(AuthRequiredMixin, SuccessMessageMixin, UpdateView):
     }
 
 
-class TaskDeleteView(AuthRequiredMixin, UserPermissionMixin,
+class TaskDeleteView(AuthRequiredMixin, AuthorDeletionMixin,
                      SuccessMessageMixin, DeleteView):
     """
     Delete existing task.
@@ -77,8 +77,8 @@ class TaskDeleteView(AuthRequiredMixin, UserPermissionMixin,
     model = Task
     success_url = reverse_lazy('tasks')
     success_message = _('Task successfully deleted')
-    permission_message = _('The task can be deleted only by its author')
-    permission_url = reverse_lazy('tasks')
+    author_message = _('The task can be deleted only by its author')
+    author_url = reverse_lazy('tasks')
     extra_context = {
         'title': _('Delete task'),
         'button_text': _('Yes, delete'),
