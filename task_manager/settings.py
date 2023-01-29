7293d67ff2671e44f0480981c3dc4b6a99117b77
Andrey Ivanov <ivnv.xd@gmail.com>
2023-01-29 18:51:26 +0100

    fix postgresql support

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -95,16 +95,19 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 if DEBUG:
-    db = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
 else:
-    db = dj_database_url.config(conn_max_age=600)
-
-DATABASES = {
-    'default': db
-}
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+        ),
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
