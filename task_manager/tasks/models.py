6011351151494956c0c10e6f8f4ac74950826e1c
Andrey Ivanov <ivnv.xd@gmail.com>
2023-01-29 20:08:12 +0100

    fix task deletion

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -58,5 +58,5 @@ class Task(models.Model):
 
 
 class TaskLabelRelation(models.Model):
-    task = models.ForeignKey(Task, on_delete=models.DO_NOTHING)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
     label = models.ForeignKey(Label, on_delete=models.PROTECT)
